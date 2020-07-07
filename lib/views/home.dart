9dfd4ac4122d59f604c2c1e237fab57a1d461d02
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-07 16:03:01 +0530

    Create AppBar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -9,7 +9,24 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Flutter",
+            ),
+            Text(
+              "News",
+              style: TextStyle(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+        elevation: 0.0,
+        centerTitle: true,
+      ),
     );
   }
 }
