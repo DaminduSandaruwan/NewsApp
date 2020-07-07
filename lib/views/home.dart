e1dc7b2b64b7946a46855f86b63ee9dc79e512a4
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-07 17:02:14 +0530

    Category Section Done

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -41,6 +41,7 @@ class _HomeState extends State<Home> {
         child: Column(
           children: <Widget>[
             Container(
+              padding: EdgeInsets.symmetric(horizontal:16),
               height: 70,
               child: ListView.builder(
                 itemCount: categories.length,
@@ -67,12 +68,34 @@ class CategoryTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(right:16),
       child: Stack(
         children: <Widget>[
-          Image.network(
-            imageUrl,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              imageUrl,
+              width: 120,
+              height: 60,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
             width: 120,
             height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              color: Colors.black26,
+            ),
+            child: Text(
+              categoryName,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ],
       ),
