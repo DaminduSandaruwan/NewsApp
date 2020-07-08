946d2dafe1a3de557961f0fd9971171ef567f68f
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-08 22:38:58 +0530

    getting data from API

---
+++ lib/helper/news.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:news_app/models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News {
+  List<ArticleModel> news =[
+    
+  ];
+
+  Future<void> getNews() async{
+    String url ="http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=6568af3c82d24a609133ab5e07393efc";
+
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if(jsonData['status']=="ok"){
+      jsonData['articles'].forEach((element){
+        if(element["urlToImage"] != null && element["description"] != null){
+          ArticleModel articleModel = ArticleModel(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            publishedAt: element['publishedAt'],
+            content: element['content']
+          );
+        }
+      });
+    }
+  }
+
+  
+}
