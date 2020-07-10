16deb2cc4be23ba7e8881985a3dd27e61bd0af07
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-07-10 18:48:19 +0530

    Creating category news class

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -6,7 +6,7 @@ class News {
   List<ArticleModel> news =[];
 
   Future<void> getNews() async{
-    String url ="http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=6568af3c82d24a609133ab5e07393efc";
+    String url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=6568af3c82d24a609133ab5e07393efc";
 
     var response = await http.get(url);
     var jsonData = jsonDecode(response.body);
@@ -26,7 +26,33 @@ class News {
         }
       });
     }
-  }
+  }  
+}
 
-  
+
+class CategoryNews {
+  List<ArticleModel> news =[];
+
+  Future<void> getNews(String category) async{
+    String url ="http://newsapi.org/v2/top-headlines?category=$category&country=in&category=business&apiKey=6568af3c82d24a609133ab5e07393efc";
+
+    var response = await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    if(jsonData['status']=="ok"){
+      jsonData['articles'].forEach((element){
+        if(element["urlToImage"] != null && element["description"] != null){
+          ArticleModel articleModel = ArticleModel(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            // publishedAt: element['publishedAt'],
+            content: element['contents']
+          );
+          news.add(articleModel);
+        }
+      });
+    }
+  }  
 }
